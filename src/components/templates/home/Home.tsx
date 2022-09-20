3807f9ee0f8fc7346c307ffc8e0c9da2c4d4450d
myron05 <85976759+myron05@users.noreply.github.com>
2022-09-19 19:14:11 -0500

    Update Home.tsx

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -3,7 +3,7 @@
 const Home = () => {
   return (
 
-    <iframe src="https://xn--digilnd-w3a.com/furvin-appe/" style={{position:"fixed", top:"60px", left:"0px", bottom:"0px", right:"0px", width:"100%", height:"100%", border:"none", margin:"0", padding:"0", overflow:"hidden"}}>
+    <iframe src="https://ludonft.io/ludo-app/" style={{position:"fixed", top:"60px", left:"0px", bottom:"0px", right:"0px", width:"100%", height:"100%", border:"none", margin:"0", padding:"0", overflow:"hidden"}}>
 </iframe>
 
 
