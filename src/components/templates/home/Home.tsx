1153a41bc01e3da5a215bc660ad65a87c6ba51c6
myron05 <85976759+myron05@users.noreply.github.com>
2022-09-20 16:14:52 -0500

    Update Home.tsx

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -3,7 +3,7 @@
 const Home = () => {
   return (
 
-    <iframe src="https://ludonft.io/ludo-app-2/" style={{position:"fixed", top:"60px", left:"0px", bottom:"0px", right:"0px", width:"100%", height:"100%", border:"none", margin:"0", padding:"0", overflow:"hidden"}}>
+    <iframe src="https://ludonft.io/ludo-app-3/" style={{position:"fixed", top:"60px", left:"0px", bottom:"0px", right:"0px", width:"100%", height:"100%", border:"none", margin:"0", padding:"0", overflow:"hidden"}}>
 </iframe>
 
 
